7025da7ba62360d99dfe61962ff955724e054356
TBoileau <t-boileau@email.com>
2021-11-23 11:01:50 +0100

    build: generate initial migration

---
+++ litreview/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LitReviewConfig(AppConfig):
+    name = 'litreview'
--- litreview/asgi.py
+++ litreview/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI src for src project.
+ASGI litreview for litreview project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'litreview.settings')
 
 application = get_asgi_application()
